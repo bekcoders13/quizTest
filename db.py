672478a33c4f2757fab1387e02e7eea6b39997b4
Asilbek <a4644145@gmail.com>
2024-09-11 14:34:32 +0500

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import pymysql
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///./database.db",
+engine = create_engine("sqlite:///./test.db",
                        connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 
